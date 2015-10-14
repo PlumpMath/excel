67bddf1fbace9c60caa90856f35c586ffc81b548
martsve <martsve@gmail.com>
2015-10-14 14:41:17 +0200

    new project for unit testing

---
+++ ExcelUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelMacro;
+
+namespace ExcelUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
